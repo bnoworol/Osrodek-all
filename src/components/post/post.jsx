b0abd7fa33d3cd1b3eedda49608fc0a35a48ffd3
Bartosz <noworolbartosz@gmail.com>
2023-12-14 14:43:27 +0100

    i take work in home

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,4 +1,4 @@
-import './post.css'
+import './post.css';
 import img1 from '../../assets/klub1.jpg';
 import img2 from '../../assets/klub3.jpg';
 import img3 from '../../assets/klub5.jpg';
@@ -10,12 +10,22 @@ export function Posts() {
 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illo ipsam quasi, repellendus eos nobis maxime quo, quas quam ab itaque placeat quibusdam enim quae cumque?',
 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illo ipsam quasi, repellendus eos nobis maxime quo, quas quam ab itaque placeat quibusdam enim quae cumque?',
 	];
-    const thumbnail = ['klub1.jpg','../../assets/klub3.jpg','../../assets/klub5.jpg']
+	const thumbnail = [
+		'https://lzs.info.pl/wp-content/uploads/2023/12/klub16-Copy.jpg',
+		'https://lzs.info.pl/wp-content/uploads/2023/12/klub5-Copy.jpg',
+		'https://lzs.info.pl/wp-content/uploads/2023/12/klub6-Copy.jpg',
+	];
+	let cost;
 	let posts = [];
 	for (let i = 0; i < title.length; i++) {
+		if (thumbnail[i] === undefined) {
+			thumbnail[i] = 'http://lzs.info.pl/wp-content/uploads/2023/01/fb_tb.png';
+		} else {
+			thumbnail[i] = thumbnail[i];
+		}
 		posts.push(
 			<div key={i} class='post'>
-				<img src={img1} alt='thumbnail' className='post_thumbnail'></img>
+				<img src={thumbnail[i]} alt='thumbnail' className='post_thumbnail'></img>
 				<div className='post_content'>
 					<h2 className='post_title'>{title[i]}</h2>
 					<p className='post_content_text'>{content[i]}</p>
